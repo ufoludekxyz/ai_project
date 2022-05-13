4c09f231209e33ff8cf72355890426b1b26d142f
ufoludekxyz <ufoludek007@gmail.com>
2022-05-13 22:35:23 +0200

    Added attributes names

--- data.py
+++ data.py
@@ -1,7 +1,15 @@
-import csv
-# import numpy as np
+import pandas as pd
 
-with open ('./diagnosis.data', 'r', encoding='utf-16') as file:
-    reader = csv.reader(file, delimiter='\t')
-    for row in reader:
-        print(row)
+par_names = ['Temperature of patient',
+             'Occurrence of nausea',
+             'Lumbar pain',
+             'Urine pushing',
+             'Micturition pains',
+             'Burning of urethra, itch, swelling of urethra outlet',
+             'decision: Inflammation of urinary bladder',
+             'decision: Nephritis of renal pelvis origin'
+             ]
+
+data_list = pd.read_csv('./diagnosis.data', sep='\t', encoding='utf-16', header=None, names=par_names)
+
+data_list = data_list.replace({'no': 0, 'yes': 1})

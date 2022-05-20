a272b22d705ffa8dbf04555adad6e6f84e498e7a
ufoludekxyz <ufoludek007@gmail.com>
2022-05-20 19:21:38 +0200

    Separate script just for data handling

---
+++ data.py
@@ -0,0 +1,43 @@
+import numpy as np
+from csv import reader
+from tabulate import tabulate
+
+# Import function
+def dataImport(name):
+    with open(name, 'r', encoding='utf-16') as file:
+        return [line for line in reader(file, delimiter='\t')]
+
+# Normalization function
+def normalizeMinMax(table):
+    for row in range(0, len(table)):
+        min_val, max_val = min(table[row]), max(table[row])
+        table[row] = [(1 - 0) * (col - min_val) / (max_val - min_val) for col in table[row]]
+    return table
+
+# Data loader function
+def loadData():
+    # Import acute.tsv to dataFile
+    dataFile = dataImport('acute.tsv')
+
+    # Create numpy array from dataList
+    dataFile = np.array(dataFile)
+
+    # Convert array of strings to array of floats
+    dataFile = dataFile.astype(float)
+
+    # Data normalization
+    dataFile = normalizeMinMax(dataFile.T).T
+
+    # Data splitting into training and test data
+    mask = np.random.rand(len(dataFile)) <= 0.8
+    trainData = dataFile[mask]
+    testData = dataFile[~mask]
+
+    # Splitting data into 2 groups, inputData (first 6 columns) and outputdata (last 2 columns)
+    testIn, testOut = testData[:,:6], testData[:,6:]
+    trainIn, trainOut = trainData[:,:6], trainData[:,6:]
+
+    # Combining inputData and outputData in a single tuple
+    trainData = [(np.array(trainIn[i], ndmin=2).T, np.array(trainOut[i], ndmin=2).T) for i in range(0, len(trainOut))]
+
+    return (trainData, testData)

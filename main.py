e911f4dcf9fde01bd9c39b5e41a4ccf741b49d92
ufoludekxyz <ufoludek007@gmail.com>
2022-05-20 22:17:14 +0200

    Playing with parameters

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import network
 trainData, testData = data.loadData()
 
 # [attributes, hidden neurons, output]
-net = network.Network([6, 3, 2])
+net = network.Network([6, 1, 2])
 
 # (training_data, epochs, batch_size, eta, test_data)
 #net.SGD(trainData, 2000, 10, 0.9, test_data=None)
-net.SGD(trainData, 200, 10, 0.9, test_data=testData)
+net.SGD(trainData, 10000, 10, 3, test_data=testData)

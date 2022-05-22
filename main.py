20c8b5ff8a9eb7d94cf676dab12000eb6bd4f769
ufoludekxyz <ufoludek007@gmail.com>
2022-05-22 13:36:54 +0200

    General testing

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import data
 import network
 
+import numpy as np
+
 trainData, testData = data.loadData()
 
 # [input vector size, S1 neurons, S2 neurons, output]
-net = network.Network([6, 2])
+net = network.Network([6, 2, 2])
 
 # (training_data, epochs, batch_size, eta, test_data)
 #net.SGD(trainData, 100, 10, 0.9, test_data=None)
-net.SGD(trainData, 1000, len(trainData), 0.9, test_data=testData)
+
+"""
+for j in range(10, len(trainData), 10):
+    print("Batch size: ", j)
+    for i in np.arange(0.01, 10, 0.01):
+        print("Eta: ", i)
+"""
+
+net.SGD(trainData, 50, 10, 0.9, test_data=testData)

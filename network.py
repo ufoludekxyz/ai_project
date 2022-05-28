f9509ab8f5ace8f5f576191eb4c258a223e20398
ufoludekxyz <ufoludek007@gmail.com>
2022-05-28 14:40:01 +0200

    Changes

--- network.py
+++ network.py
@@ -30,9 +30,6 @@ class Network(object):
     def SGD(self, training_data, epochs, mini_batch_size, eta,
             test_data=None):
 
-        #Opening file for output
-        file = open('experiments/experiments_%s.tsv'%epochs, 'a', encoding='utf-8') 
-
         """Train the neural network using mini-batch stochastic
         gradient descent.  The ``training_data`` is a list of tuples
         ``(x, y)`` representing the training inputs and the desired
@@ -41,6 +38,7 @@ class Network(object):
         network will be evaluated against the test data after each
         epoch, and partial progress printed out.  This is useful for
         tracking progress, but slows things down substantially."""
+
         if test_data: n_test = len(test_data)
         n = len(training_data)
         for j in range(epochs):
@@ -57,16 +55,6 @@ class Network(object):
             if test_data:
                 print("Epoch {0}: {1} / {2} - {3:.0f}%, {4:.2f}s".format(
                     j, evalVal, n_test, evalAcc, time2-time1))
-                #print("{0}, {3:.0f}".format(
-                #    j, self.evaluate(test_data), n_test, ((self.evaluate(test_data)/n_test)*100), time2-time1))
-
-                #Formatting output
-                #file.write('%s\t%s\t%s\t%s\n'%(epochs, mini_batch_size, eta, evalAcc))
-
-                S1 = self.sizes[1]
-                S2 = self.sizes[2]
-                file.write('%s\t%s\t%s\n'%(S1, S2, evalAcc))
-
             else:
                 print("Epoch {0} complete in {1:.2f} seconds".format(j, time2-time1))
 

9f10ec761d91669e7780a75cb842b786fedef8b0
ufoludekxyz <ufoludek007@gmail.com>
2022-05-28 17:34:36 +0200

    Added target error

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import numpy as np
 trainData, testData = data.loadData()
 
 # [input vector size, S1 neurons, S2 neurons, output]
-#net = network.Network([6, 2, 2])
+net = network.Network([6, 2])
 
-# (training_data, epochs, batch_size, eta, test_data)
+# (training_data, epochs, batch_size, eta, target, test_data)
 #net.SGD(trainData, 100, 10, 0.9, test_data=None)
+net.SGD(trainData, 10000, 10, 0.9, error_target=0.01,test_data=testData)
 
-for i in np.arange(1, 20, 1):
-    for j in np.arange(1, 20, 1):
-        net = network.Network([6, i, j, 2])
-        net.SGD(trainData, 1000, 10, 0.9, test_data=testData)
+#for i in np.arange(1, 20, 1):
+#    for j in np.arange(1, 20, 1):
+#        net = network.Network([6, i, j, 2])
+#        net.SGD(trainData, 1000, 10, 0.9, test_data=testData)
--- network.py
+++ network.py
@@ -5,16 +5,7 @@ import numpy as np
 class Network(object):
 
     def __init__(self, sizes):
-        """The list ``sizes`` contains the number of neurons in the
-        respective layers of the network.  For example, if the list
-        was [2, 3, 1] then it would be a three-layer network, with the
-        first layer containing 2 neurons, the second layer 3 neurons,
-        and the third layer 1 neuron.  The biases and weights for the
-        network are initialized randomly, using a Gaussian
-        distribution with mean 0, and variance 1.  Note that the first
-        layer is assumed to be an input layer, and by convention we
-        won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers."""
+
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
@@ -22,22 +13,17 @@ class Network(object):
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
     def feedforward(self, a):
-        """Return the output of the network if ``a`` is input."""
+
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta,
-            test_data=None):
+    def mse(self,_test_data):
+        error=[pow(np.linalg.norm(self.feedforward(x)-y),2) for (x,y) in _test_data]
+        return 1/len(_test_data)*sum(error)
 
-        """Train the neural network using mini-batch stochastic
-        gradient descent.  The ``training_data`` is a list of tuples
-        ``(x, y)`` representing the training inputs and the desired
-        outputs.  The other non-optional parameters are
-        self-explanatory.  If ``test_data`` is provided then the
-        network will be evaluated against the test data after each
-        epoch, and partial progress printed out.  This is useful for
-        tracking progress, but slows things down substantially."""
+    def SGD(self, training_data, epochs, mini_batch_size, eta,
+            error_target=0.001, test_data=None):
 
         if test_data: n_test = len(test_data)
         n = len(training_data)
@@ -49,20 +35,22 @@ class Network(object):
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
+            cur_err = self.mse(training_data)
             time2 = time.time()
             evalVal = self.evaluate(test_data)
             evalAcc = (evalVal/n_test*100)
-            if test_data:
-                print("Epoch {0}: {1} / {2} - {3:.0f}%, {4:.2f}s".format(
-                    j, evalVal, n_test, evalAcc, time2-time1))
-            else:
-                print("Epoch {0} complete in {1:.2f} seconds".format(j, time2-time1))
+            if cur_err < error_target or j == epochs-1:
+                if test_data:
+                    print("Epoch {0}: {1} / {2} - {3:.0f}%".format(
+                        j, evalVal, n_test, evalAcc))
+                    #print("Epoch {0}: {1} / {2} - {3:.0f}%".format(
+                    #    j, evalVal, n_test, evalAcc '''time2-time1'''))
+                else:
+                    print("Epoch {0} complete in {1:.2f} seconds".format(j, time2-time1))
+                break
 
     def update_mini_batch(self, mini_batch, eta):
-        """Update the network's weights and biases by applying
-        gradient descent using backpropagation to a single mini batch.
-        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
-        is the learning rate."""
+
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
@@ -75,12 +63,10 @@ class Network(object):
                        for b, nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
-        """Return a tuple ``(nabla_b, nabla_w)`` representing the
-        gradient for the cost function C_x.  ``nabla_b`` and
-        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
+
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+
         # feedforward
         activation = x
         activations = [x] # list to store all the activations, layer by layer
@@ -90,17 +76,13 @@ class Network(object):
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
+
         # backward pass
         delta = self.cost_derivative(activations[-1], y) * \
             sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-        # Note that the variable l in the loop below is used a little
-        # differently to the notation in Chapter 2 of the book.  Here,
-        # l = 1 means the last layer of neurons, l = 2 is the
-        # second-last layer, and so on.  It's a renumbering of the
-        # scheme in the book, used here to take advantage of the fact
-        # that Python can use negative indices in lists.
+
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
@@ -110,27 +92,21 @@ class Network(object):
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data):
-        """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
+
         test_results = [(self.feedforward(x), y)
                         for (x, y) in test_data]
+
                         # spychology, 
         return sum(int((y[0] == 0 and x[0] <= 0.3) or (y[0] == 1 and x[0] >= 0.7) and 
                        (y[1] == 0 and x[1] <= 0.3) or (y[1] == 1 and x[1] >= 0.7)) 
                    for (x, y) in test_results)
 
     def cost_derivative(self, output_activations, y):
-        """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
         return (output_activations-y)
 
 #### Miscellaneous functions
 def sigmoid(z):
-    """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
 
 def sigmoid_prime(z):
-    """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
